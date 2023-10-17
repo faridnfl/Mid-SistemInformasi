1ea2460e39f5226f2d30ba665e702165cfc6c162
faridnfl <faridnaufal34@gmail.com>
2023-10-17 22:29:53 +0800

    semula

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <header style="background-color: rgb(19, 58, 98); padding: 20px 0;">
-    <img src="/unhas_id.png" alt="logo" style=" padding-left: 20px; height: 100px; margin-right: 20px;"  />
+    <img src="/unhas_id.png" alt="logo" style=" padding-left: 20px; height: 50px; margin-right: 20px;"  />
   </header>
 </template>
