01eab9b863aca1522b9e7d92baf001d78735b755
faridnfl <faridnaufal34@gmail.com>
2023-10-18 19:15:36 +0800

    farid

--- app.vue
+++ app.vue
@@ -1,24 +1,24 @@
 <template>
   <header style="background-color: rgb(19, 58, 98); padding: 15px 15px; height: 90px;">
     <div class="header-logo">
-      <img src="/unhas_id.png" alt="logo" style="height: 50px" />
+      <img src="/unhas_id.png" alt="logo" style="height: 35px; width: 250px;" />
     </div>
-    <div class="header-dropdown" >
-        <p style="font-family: Roboto;  color: white; font-size: 20px; float: left; margin-left: 30px;">PROFIL</p>
-        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">AKADEMIK</p>
-        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">SUMBER DAYA</p>
-        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">KEMAHASISWAAN</p>
-        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">RISET DAN INOVASI</p>
-        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">KEMITRAAN</p>
-        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">GPM-PR</p>
-        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">COT</p>
-        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">DHARMA WANITA</p>
+    <div class="header-dropdown"  >
+        <p style="font-family: Roboto;  color: white; font-size: 16px; float: left; margin-left: 10px;">PROFIL</p>
+        <p style="font-family: Roboto; color: white; font-size: 16px; float: left; margin-left: 10px;">AKADEMIK</p>
+        <p style="font-family: Roboto; color: white; font-size: 16px; float: left; margin-left: 10px;">SUMBER DAYA</p>
+        <p style="font-family: Roboto; color: white; font-size: 16px; float: left; margin-left: 10px;">KEMAHASISWAAN</p>
+        <p style="font-family: Roboto; color: white; font-size: 16px; float: left; margin-left: 10px;">RISET DAN INOVASI</p>
+        <p style="font-family: Roboto; color: white; font-size: 16px; float: left; margin-left: 10px;">KEMITRAAN</p>
+        <p style="font-family: Roboto; color: white; font-size: 16px; float: left; margin-left: 10px;">GPM-PR</p>
+        <p style="font-family: Roboto; color: white; font-size: 16px; float: left; margin-left: 10px;">COT</p>
+        <p style="font-family: Roboto; color: white; font-size: 16px; float: left; margin-left: 10px;">DHARMA WANITA</p>
     </div>
     <div class="header-language">
-        <p style="font-family: Roboto; color: white; float: left; margin-top:4px;">INA</p>
-        <img src="/indonesia-flag.png" alt="indo" style="float: left; width: 33px; height: 30px; margin-left: 5px;" />
-        <p style="font-family: Roboto; color: white; float: left; margin-top:4px; margin-left: 10px;">ENG</p>
-        <img src="/english.png" alt="english" style="float: left; width: 30px; height: 20px; margin-top:4px; margin-left: 5px" />
+        <p style="font-family: Roboto; color: white; float: left; margin-top:4px; font-size: 12px;">INA</p>
+        <img src="/indonesia-flag.png" alt="indo" style="float: left; width: 25px; height: 25px; margin-left: 5px;" />
+        <p style="font-family: Roboto; color: white; float: left; margin-top:4px; margin-left: 10px; font-size: 12px;">ENG</p>
+        <img src="/english.png" alt="english" style="float: left; width: 25px; height: 15px; margin-top:4px; margin-left: 5px" />
     </div>
   </header>
 </template>
@@ -28,18 +28,19 @@
 <style scoped>
 .header-logo {
   float: left;
-
+  margin-top: 7px;
 }
 
 .header-dropdown {
-  width: 1340px;
+  width: 930px;
   float: left;
-  margin: 12px;
+  margin-top: 17px;
+  margin-left: 10px;
 }
 
 .header-language {
-  width: 143px;
+  width: 120px;
   float: left;
-  margin: 12px 5px;
+  margin: 15px 10px;
 }
 </style>
