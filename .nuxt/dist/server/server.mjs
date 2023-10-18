6649098251ca8059d5c64238951144b2f8e7663a
faridnfl <faridnaufal34@gmail.com>
2023-10-18 17:34:08 +0800

    farid

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///D:/Kuliah/Semester%205/Tugas/Sistem%20Informasi/Tugas_MID_SISFOR/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- app.vue
+++ app.vue
@@ -1,5 +1,45 @@
 <template>
-  <header style="background-color: rgb(19, 58, 98); padding: 20px 0;">
-    <img src="/unhas_id.png" alt="logo" style=" padding-left: 20px; height: 50px; margin-right: 20px;"  />
+  <header style="background-color: rgb(19, 58, 98); padding: 15px 15px; height: 90px;">
+    <div class="header-logo">
+      <img src="/unhas_id.png" alt="logo" style="height: 50px" />
+    </div>
+    <div class="header-dropdown" >
+        <p style="font-family: Roboto;  color: white; font-size: 20px; float: left; margin-left: 30px;">PROFIL</p>
+        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">AKADEMIK</p>
+        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">SUMBER DAYA</p>
+        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">KEMAHASISWAAN</p>
+        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">RISET DAN INOVASI</p>
+        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">KEMITRAAN</p>
+        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">GPM-PR</p>
+        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">COT</p>
+        <p style="font-family: Roboto; color: white; font-size: 20px; float: left; margin-left: 30px;">DHARMA WANITA</p>
+    </div>
+    <div class="header-language">
+        <p style="font-family: Roboto; color: white; float: left; margin-top:4px;">INA</p>
+        <img src="/indonesia-flag.png" alt="indo" style="float: left; width: 33px; height: 30px; margin-left: 5px;" />
+        <p style="font-family: Roboto; color: white; float: left; margin-top:4px; margin-left: 10px;">ENG</p>
+        <img src="/english.png" alt="english" style="float: left; width: 30px; height: 20px; margin-top:4px; margin-left: 5px" />
+    </div>
   </header>
 </template>
+
+
+
+<style scoped>
+.header-logo {
+  float: left;
+
+}
+
+.header-dropdown {
+  width: 1340px;
+  float: left;
+  margin: 12px;
+}
+
+.header-language {
+  width: 143px;
+  float: left;
+  margin: 12px 5px;
+}
+</style>
