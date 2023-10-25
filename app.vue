7dcb7af3cc0c4466a573eaa6eea581d46d564b5b
iqoypresto <rfqimaulana888@gmail.com>
2023-10-25 19:15:08 +0800

    add footer

--- app.vue
+++ app.vue
@@ -127,7 +127,68 @@
         <h1 class="text-2xl font-bold py-5 text-center">Disini Site Map</h1>
       </div>
     </section>
-    
+    <footer>
+      <div class="content max-w-screen-xl mx-auto py-20">
+        <div class="grid grid-cols-3 gap-5 text-sm font-semibold">
+          <div>
+            <h3>ALAMAT</h3>
+            <div class="ms-10 my-6 text-gray-400 font-normal">
+              <p>Jalan Poros Malino Km. 6, Bontomarannu. Gowa, Sulawesi Selatan. 92171</p>
+            </div>
+            <h3>KONTAK & EMAIL</h3>
+            <div class="ms-10 my-6 text-gray-400 font-normal">
+              <p>+62 852-9999-9958</p>
+              <p>teknik@unhas.ac.id</p>
+            </div>
+            <img src="./unhas_id.png" alt="">
+            <h2 class="mt-6">SITE STATISTICS</h2>
+            <div class="my-6 text-gray-400 font-normal">
+              <a href="">
+                <img src="" alt="Flag Counter">
+              </a>
+            </div>
+          </div>
+          <div>
+            <h3>INFORMASI</h3>
+            <div class="ms-10 my-6 text-gray-400 font-normal">
+              <a href="">
+                <p>THE 3RD EPI INTERNATIONAL CONFERENCE ON SCIENCE AND ENGINEERING 2019</p>
+                <p class="text-xs mt-2">23 September 2019</p>
+              </a>
+              <div class="mt-5">
+                <p>WORKSHOP & SIMPOSIUM NASIONAL GEOMEKANIKA 5 2019</p>
+                <p class="text-xs mt-2">06 Agustus 2019</p>
+              </div>
+              <div class="mt-5">
+                <p>Pengembangan Karakter Mahasiswa Baru 2019 Fakultas Teknik Unhas</p>
+                <p class="text-xs mt-2">09 Juli 2019</p>
+              </div>
+              <div class="mt-5">
+                <p>International Guest Lecture on Journal Article</p>
+                <p class="text-xs mt-2">25 Juni 2019</p>
+              </div>
+            </div>
+            <h3>PELAYANAN PUBLIK</h3>
+          </div>
+          <div>
+            <h3>PENELUSURAN OLEH GOOGLE</h3>
+            <form class="mt-6 bg-white p-3 flex justify-between" action="https://www.google.com/search" method="GET">
+              <input type="text" name="q" class="text-black p-1 w-full border" placeholder="Google Search">
+              <input type="submit" value="Search" class="bg-black px-3">
+            </form>
+            <h3 class="mt-6">DIDUKUNG OLEH</h3>
+            <div class="mt-6 flex flex-col gap-3 items-center">
+              <img class="bg-white" width="100px" src="https://eng.unhas.ac.id/fakultas/uploads/logo/58848eafe9ed984f2de7512fcf09b24a.png" alt="">
+              <img class="bg-white" width="100px" src="https://eng.unhas.ac.id/fakultas/uploads/logo/b93dfcc51ffc4fee783d7218ae4dfa41.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="border-black">
+      <div class="py-6 text-center text-gray-400 text-sm">
+        Copyright © 2023 All rights reserved
+      </div>
+    </footer>
   </body>
 </template>
 
@@ -178,5 +239,10 @@ header .navbar a:hover{
 .dharma-wanita button {
   padding: 0.8rem 2.5rem;
   border-radius: 10px;
+}
+
+footer {
+  background-color: #063d63;
+  color: white;
 }
 </style>
